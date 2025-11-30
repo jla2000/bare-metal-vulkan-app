8b6c225aa32eebb3eed38fc84f75187a50839497
Jan Lafferton <jan@lafferton.de>
2025-11-30 11:55:16 +0100

    instance

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ const common = @import("common.zig");
 const queue = @import("queue.zig");
 const swap = @import("swapchain.zig");
 const dev = @import("device.zig");
+const inst = @import("instance.zig");
 const c = common.c;
 
 const allocator = common.allocator;
@@ -21,7 +22,7 @@ pub fn main() !void {
     const window = c.glfwCreateWindow(800, 600, "Vulkan window", null, null);
     defer c.glfwDestroyWindow(window);
 
-    const instance = create_instance();
+    const instance = inst.create_instance();
     defer c.vkDestroyInstance(instance, null);
 
     var debug_messenger = debug.create_debug_messenger(instance);
@@ -84,42 +85,4 @@ fn create_shader_module(device: c.VkDevice, code: []const u32) c.VkShaderModule 
     assert(c.vkCreateShaderModule(device, &create_info, null, &shader_module) == c.VK_SUCCESS);
 
     return shader_module;
-}
-
-fn create_instance() c.VkInstance {
-    var num_glfw_extensions: u32 = 0;
-    var glfw_extensions: [*c][*c]const u8 = null;
-    glfw_extensions = c.glfwGetRequiredInstanceExtensions(&num_glfw_extensions);
-
-    var extensions = allocator.alloc([*c]const u8, num_glfw_extensions + 1) catch unreachable;
-    defer allocator.free(extensions);
-
-    @memcpy(extensions, glfw_extensions);
-    extensions[extensions.len - 1] = c.VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
-
-    const enabled_layers = [_][*]const u8{"VK_LAYER_KHRONOS_validation"};
-
-    const app_info = c.VkApplicationInfo{
-        .sType = c.VK_STRUCTURE_TYPE_APPLICATION_INFO,
-        .pApplicationName = "zig-raytracer",
-        .applicationVersion = c.VK_MAKE_VERSION(0, 0, 1),
-        .pEngineName = "No engine",
-        .engineVersion = c.VK_MAKE_VERSION(0, 0, 1),
-        .apiVersion = c.VK_API_VERSION_1_4,
-    };
-
-    const instance_create_info = c.VkInstanceCreateInfo{
-        .sType = c.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
-        .pApplicationInfo = &app_info,
-        .enabledExtensionCount = @intCast(extensions.len),
-        .ppEnabledExtensionNames = extensions.ptr,
-        .enabledLayerCount = enabled_layers.len,
-        .ppEnabledLayerNames = &enabled_layers,
-        .pNext = @ptrCast(&debug.DEBUG_MESSENGER_CREATE_INFO),
-    };
-
-    var instance: c.VkInstance = undefined;
-    assert(c.vkCreateInstance(&instance_create_info, null, &instance) == c.VK_SUCCESS);
-
-    return instance;
 }
